183c3c1e7cc3a4fa6a18b4ffce8dd45559e04a38
Ihor Ivanets <ivanets22@gmail.com>
2026-01-06 22:13:53 +0200

    completed per tasks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,17 +4,49 @@ import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
 
 import css from "./App.module.css";
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../services/postService";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [debounceSearchQuery] = useDebounce(searchQuery, 300);
+
+  const { data } = useQuery({
+    queryKey: ["posts", debounceSearchQuery, currentPage],
+    queryFn: () => fetchPosts(debounceSearchQuery, currentPage),
+    placeholderData: keepPreviousData,
+  });
+
+  const handleSearchQuery = (newQuery: string) => {
+    // console.log(newQuery);
+    setSearchQuery(newQuery);
+    setCurrentPage(1);
+  };
+  // console.log(data);
+
+  // console.log(debounceSearchQuery);
+
+  const posts = data?.posts ?? [];
+  const totalPages = data?.totalCount ? Math.ceil(data.totalCount / 8) : 0;
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox />
-        <Pagination />
+        <SearchBox onSearch={handleSearchQuery} />
+        {totalPages > 1 && (
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
+        )}
         <button className={css.button}>Create post</button>
       </header>
-      <Modal>{/* Передати через children компонент CreatePostForm або EditPostForm */}</Modal>
-      <PostList />
+      {/* <Modal>Передати через children компонент CreatePostForm або EditPostForm</Modal> */}
+      {posts.length > 0 && <PostList posts={posts} />}
     </div>
   );
 }
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,11 +1,53 @@
 import axios from "axios";
+import { Post } from "../types/post";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-export const fetchPosts = async (searchText, page) => {};
+export const fetchPosts = async (
+  searchText: string,
+  page: number
+): Promise<{ posts: Post[]; totalCount: number }> => {
+  const response = await axios.get<Post[]>("/posts", {
+    params: {
+      ...(searchText !== "" && { q: searchText }),
+      _page: page,
+      _limit: 8,
+    },
+  });
 
-export const createPost = async (newPost) => {};
+  const totalCount = Number(response.headers["x-total-count"]);
 
-export const editPost = async (newDataPost) => {};
+  return {
+    posts: response.data,
+    totalCount,
+  };
+};
 
-export const deletePost = async (postId) => {};
+interface NewPost {
+  title: string;
+  body: string;
+}
+
+export const createPost = async (newPost: NewPost): Promise<Post> => {
+  const response = await axios.post<Post>("/posts", newPost);
+
+  return response.data;
+};
+
+interface EditPost {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export const editPost = async (newDataPost: EditPost): Promise<Post> => {
+  const response = await axios.patch<Post>(`/posts/${newDataPost.id}`, newDataPost);
+
+  return response.data;
+};
+
+export const deletePost = async (postId: number): Promise<Post> => {
+  const response = await axios.delete<Post>(`/posts/${postId}`);
+
+  return response.data;
+};
